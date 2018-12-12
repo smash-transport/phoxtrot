ced5571a46eb31c2ed0433ae699999b871b2ed40
Anna Schäfer <aschaefer@fias.uni-frankfurt.de>
2018-12-12 08:49:00 +0100

    Run clang-format

--- src/include/total_Xsections.h
+++ src/include/total_Xsections.h
@@ -7,30 +7,28 @@
 #include <fstream>
 #include <iostream>
 
-
-//C11: (π + ρ0 -> (π, ρ, a1) -> π + γ)
+// C11: (π + ρ0 -> (π, ρ, a1) -> π + γ)
 double inline total_xsection_C11(double s);
 
-//C12: (π0 + ρ -> (π, ρ, a1) -> π + γ)
+// C12: (π0 + ρ -> (π, ρ, a1) -> π + γ)
 double inline total_xsection_C11(double s);
 
-//C13: (π + ρ -> (π, ρ, a1) -> π0 + γ)
+// C13: (π + ρ -> (π, ρ, a1) -> π0 + γ)
 double inline total_xsection_C11(double s);
 
-//C14: (π0 + ρ0 -> (ω) -> π0 + γ)
+// C14: (π0 + ρ0 -> (ω) -> π0 + γ)
 double inline total_xsection_C11(double s);
 
-//C15: (π + ρ -> (ω) -> π0 + γ)
+// C15: (π + ρ -> (ω) -> π0 + γ)
 double inline total_xsection_C11(double s);
 
-//C16: (π0 + ρ -> (ω) -> π + γ)
+// C16: (π0 + ρ -> (ω) -> π + γ)
 double inline total_xsection_C11(double s);
 
-//C21: (π + π -> (π, ρ, a1) -> ρ0 + γ)
+// C21: (π + π -> (π, ρ, a1) -> ρ0 + γ)
 double inline total_xsection_C11(double s);
 
-//C22: (π + π0 -> (π, ρ, a1) -> ρ + γ)
+// C22: (π + π0 -> (π, ρ, a1) -> ρ + γ)
 double inline total_xsection_C11(double s);
-
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -9,14 +9,14 @@
 
 int main() {
   std::cout << "Checking total xsection:" << std::endl;
-  double sigma_C11 = total_xsection_C11(0.994*0.994);
-  double sigma_C12 = total_xsection_C12(0.994*0.994);
-  double sigma_C13 = total_xsection_C13(0.994*0.994);
-  double sigma_C14 = total_xsection_C14(0.994*0.994);
-  double sigma_C15 = total_xsection_C15(0.994*0.994);
-  double sigma_C16 = total_xsection_C16(0.994*0.994);
-  double sigma_C21 = total_xsection_C21(0.994*0.994);
-  double sigma_C22 = total_xsection_C22(0.994*0.994);
+  double sigma_C11 = total_xsection_C11(0.994 * 0.994);
+  double sigma_C12 = total_xsection_C12(0.994 * 0.994);
+  double sigma_C13 = total_xsection_C13(0.994 * 0.994);
+  double sigma_C14 = total_xsection_C14(0.994 * 0.994);
+  double sigma_C15 = total_xsection_C15(0.994 * 0.994);
+  double sigma_C16 = total_xsection_C16(0.994 * 0.994);
+  double sigma_C21 = total_xsection_C21(0.994 * 0.994);
+  double sigma_C22 = total_xsection_C22(0.994 * 0.994);
   std::cout << sigma_C11 << '\n';
   std::cout << sigma_C12 << '\n';
   std::cout << sigma_C13 << '\n';
@@ -29,14 +29,14 @@ int main() {
   std::cout << " " << std::endl;
 
   std::cout << "Checking differential xsection:" << std::endl;
-  double diff_sigma_C11 = diff_xsection_C11(-0.25, 0.994*0.994);
-  double diff_sigma_C12 = diff_xsection_C12(-0.25, 0.994*0.994);
-  double diff_sigma_C13 = diff_xsection_C13(-0.25, 0.994*0.994);
-  double diff_sigma_C14 = diff_xsection_C14(-0.25, 0.994*0.994);
-  double diff_sigma_C15 = diff_xsection_C15(-0.25, 0.994*0.994);
-  double diff_sigma_C16 = diff_xsection_C16(-0.25, 0.994*0.994);
-  double diff_sigma_C21 = diff_xsection_C21(-0.25, 0.994*0.994);
-  double diff_sigma_C22 = diff_xsection_C22(-0.25, 0.994*0.994);
+  double diff_sigma_C11 = diff_xsection_C11(-0.25, 0.994 * 0.994);
+  double diff_sigma_C12 = diff_xsection_C12(-0.25, 0.994 * 0.994);
+  double diff_sigma_C13 = diff_xsection_C13(-0.25, 0.994 * 0.994);
+  double diff_sigma_C14 = diff_xsection_C14(-0.25, 0.994 * 0.994);
+  double diff_sigma_C15 = diff_xsection_C15(-0.25, 0.994 * 0.994);
+  double diff_sigma_C16 = diff_xsection_C16(-0.25, 0.994 * 0.994);
+  double diff_sigma_C21 = diff_xsection_C21(-0.25, 0.994 * 0.994);
+  double diff_sigma_C22 = diff_xsection_C22(-0.25, 0.994 * 0.994);
   std::cout << diff_sigma_C11 << '\n';
   std::cout << diff_sigma_C12 << '\n';
   std::cout << diff_sigma_C13 << '\n';
@@ -46,5 +46,4 @@ int main() {
   std::cout << diff_sigma_C21 << '\n';
   std::cout << diff_sigma_C22 << '\n';
   std::cout << "Check :)" << std::endl;
-
 }
